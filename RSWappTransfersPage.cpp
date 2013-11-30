062c33feb7c51bfa7da0c34de92fe3ba058414cd
csoler <csoler@users.sourceforge.net>
2013-11-30 20:02:42 +0100

    added parsing/DL of RSLinks

--- RSWappTransfersPage.cpp
+++ RSWappTransfersPage.cpp
@@ -1,7 +1,12 @@
-
+#include <stdio.h>
 #include <retroshare/rsfiles.h>
 
 #include <Wt/WTimer>
+#include <Wt/WText>
+#include <Wt/WVBoxLayout>
+#include <Wt/WTextArea>
+#include <Wt/WMessageBox>
+#include <Wt/WPushButton>
 #include <Wt/WContainerWidget>
 #include <Wt/WAbstractTableModel>
 
@@ -146,6 +151,9 @@ RSWappTransfersPage::RSWappTransfersPage(Wt::WContainerWidget *parent,RsFiles *m
 
 	Wt::WTableView *tableView = new Wt::WTableView(_impl);
 
+	Wt::WVBoxLayout *layout = new Wt::WVBoxLayout() ;
+	_impl->setLayout(layout) ;
+
 	tableView->setAlternatingRowColors(true);
 
 	//tableView->setModel(fileFilterModel_);
@@ -159,6 +167,15 @@ RSWappTransfersPage::RSWappTransfersPage(Wt::WContainerWidget *parent,RsFiles *m
 	tableView->setColumnWidth(5, 100);
 
 	tableView->setModel(new DownloadsTransfersListModel(mfiles)) ;
+	layout->addWidget(tableView) ;
+
+	link_area = new Wt::WTextArea(_impl) ;
+	link_area->setText("Paste Retroshare links here to download them,\nand press Download.") ;
+	layout->addWidget(link_area) ;
+
+	Wt::WPushButton *btn = new Wt::WPushButton("Parse!") ;
+	btn->clicked().connect(this,&RSWappTransfersPage::downloadLink) ;
+	layout->addWidget(btn) ;
 
 	_timer = new Wt::WTimer(this) ;
 
@@ -166,3 +183,94 @@ RSWappTransfersPage::RSWappTransfersPage(Wt::WContainerWidget *parent,RsFiles *m
 	_timer->timeout().connect(tableView,&Wt::WTableView::refresh) ;
 	_timer->start() ;
 }
+
+void RSWappTransfersPage::downloadLink()
+{
+	std::string lstr = link_area->text().toUTF8() ;
+
+	std::cerr << "Parsing links:" << std::endl;
+	std::cerr << lstr << std::endl;
+
+	std::vector<std::string> names ;
+	std::vector<std::string> hashs ;
+	std::vector<uint64_t   > sizes ;
+
+	// parse the link. We can do better, and for now this is really basic stuff.
+	//
+	size_t current_position = 0 ;
+
+	while(true)
+	{
+		// look for the next occurence of "retroshare://file?"
+		
+		size_t s = lstr.find("retroshare://file?",current_position) ;
+
+		if(s == std::string::npos)
+			break ;
+
+		// split into chunks separated by &
+
+		size_t s2 = lstr.find_first_of('&',s) ;
+
+		if(s2 == std::string::npos)
+			break ;
+
+		std::string name_str = lstr.substr(s+18+std::string("name=").length(),s2-s-18-std::string("name=").length()) ;
+		
+		size_t s3 = lstr.find_first_of('&',s2+1) ;
+		if(s3 == std::string::npos)
+			break ;
+
+		uint64_t size ;
+		if(sscanf(lstr.substr(s2+1,s3-s2-1).c_str(),"size=%lld",&size) != 1)
+			break ;
+
+		size_t s4 = lstr.find_first_not_of("0123456789abcdef",s3+1+std::string("hash=").length()) ;
+
+		if(s == std::string::npos)
+			break ;
+
+		std::string hash_str = lstr.substr(s3+1+std::string("hash=").length(),s4-s3-1-std::string("hash=").length()) ;
+
+		std::cerr << "s = " << s << ", s2=" << s2 << ", s3=" << s3 << ",s4=" << s4 << std::endl;
+		std::cerr << "name: " << name_str << std::endl;
+		std::cerr << "hash: " << hash_str << std::endl;
+		std::cerr << "size: " << size << std::endl;
+
+		// extract field for name, size and hash
+		//
+		current_position = s4 ;
+
+		names.push_back(name_str) ;
+		hashs.push_back(hash_str) ;
+		sizes.push_back(size) ;
+	}
+
+	Wt::WText *out = new Wt::WText(_impl);
+	out->setMargin(10, Wt::Left);
+
+	Wt::WString num_files = Wt::WString("{1}").arg((int)names.size()) ;
+
+	Wt::WString files_str = "<ul>" ;
+	for(uint32_t i=0;i<names.size();++i)
+		files_str += "<li>" + hashs[i] + ", " + Wt::WString("{1}").arg((int)(sizes[i]/1000)) + Wt::WString("{1}").arg((int)(sizes[i]%1000)) + " bytes, name: " + names[i] + "</li>" ;
+
+	files_str += "</ul>";
+
+	Wt::StandardButton answer = Wt::WMessageBox::show("Download these files?", "<p>Download these " +num_files+" files?</p>"+files_str, Wt::Ok | Wt::Cancel);
+
+	if (answer == Wt::Ok)
+	{
+		std::list<std::string> srcids ;
+
+		for(uint32_t i=0;i<names.size();++i)
+			mFiles->FileRequest(names[i],hashs[i],sizes[i],"",RS_FILE_REQ_ANONYMOUS_ROUTING,srcids) ;
+
+		link_area->setText("") ;
+	}
+	else
+	{
+		out->setText("Waiting on your decision...");
+	}
+}
+
--- RSWappTransfersPage.h
+++ RSWappTransfersPage.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <Wt/WTableView>
+#include <Wt/WTextArea>
 
 class RSWappTransfersPage: public Wt::WCompositeWidget
 {
 	public:
 		RSWappTransfersPage(Wt::WContainerWidget *container,RsFiles *rsfiles) ;
 
+		void downloadLink() ;
+
 	private:
 		Wt::WContainerWidget *_impl;
 		RsFiles *mFiles ;
 		Wt::WTimer *_timer ;
+		Wt::WTextArea *link_area ;
 };
